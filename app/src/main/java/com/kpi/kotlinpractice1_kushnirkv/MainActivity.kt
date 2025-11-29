75653afe88ba04f19aaae02b9a59b0e2d6605415
dogmoony <k.kushnir.tv12@kpi.ua>
2025-11-29 23:08:07 +0000

    Initial Android project with Jetpack Compose.

---
+++ app/src/main/java/com/kpi/kotlinpractice1_kushnirkv/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kpi.kotlinpractice1_kushnirkv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kpi.kotlinpractice1_kushnirkv.ui.theme.KotlinPractice1KushnirKVTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinPractice1KushnirKVTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinPractice1KushnirKVTheme {
+        Greeting("Android")
+    }
+}
